af6f1ae034f14493390a060c3e90c634286bf9f4
shishpal <shishpalpolampally@gmail.com>
2026-04-02 13:47:25 +0530

    refactor: restructure project directory, update entry point, and add database and security dependencies

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const connectDB = require("./src/config/connectDB");
+const connectDB = require("./config/connectDB");
 
 dotenv.config();
 
--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGODB_URI);
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(`DB Connection Error: ${err.message}`);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
